e3accc8df666b71c671935db1477c14b8af7f451
Budiman Wijayanto <budimanwjynto@gmail.com>
2026-01-29 18:16:52 +0700

    fix(location): update map iframe to the latest version

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -249,7 +249,7 @@ export default async function HomePage() {
                     <div className="max-w-4xl mx-auto">
                         <div className="aspect-video rounded-lg overflow-hidden shadow-lg">
                             <iframe
-                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d540.2392891877454!2d107.5356125616693!3d-6.902887666887708!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68e5d2e0679eed%3A0x360a44c9ee0eb336!2sJl.%20Mahar%20Martanegara%20No.218%2C%20Utama%2C%20Kec.%20Cimahi%20Sel.%2C%20Kota%20Cimahi%2C%20Jawa%20Barat%2040533!5e1!3m2!1sid!2sid!4v1769684946929!5m2!1sid!2sid"
+                                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d990.224233580919!2d107.53565228970128!3d-6.902927518682059!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68e5d2e0679eed%3A0x360a44c9ee0eb336!2sJl.%20Mahar%20Martanegara%20No.218%2C%20Utama%2C%20Kec.%20Cimahi%20Sel.%2C%20Kota%20Cimahi%2C%20Jawa%20Barat%2040533!5e0!3m2!1sid!2sid!4v1769685139633!5m2!1sid!2sid"
                                 width="100%"
                                 height="100%"
                                 style={{ border: 0 }}
